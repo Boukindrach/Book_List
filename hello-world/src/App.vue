564fbbc1a1a9ecd125303393aa257690bd7c35fc
Boukind Rachid <boukind.r1@gmail.com>
2024-12-30 23:35:42 +0100

    adding a navbar

--- hello-world/src/App.vue
+++ hello-world/src/App.vue
@@ -1,10 +1,19 @@
 <template>
-  <h1>My App</h1>
+  <Header />
 </template>
 
 <script>
-export default {
+import Header from "./components/Header.vue"
+import Body from "./components/Body.vue"
+import Footer from "./components/Footer.vue"
 
+export default {
+  name: 'App',
+  components: {
+    Header,
+    Body,
+    Footer,
+  }
 }
 </script>
 
