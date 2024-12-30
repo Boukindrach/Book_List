08f6424e5ce819f86d562abc4a86fb2f2f380c3e
Boukind Rachid <boukind.r1@gmail.com>
2024-12-30 23:45:48 +0100

    adding the body

--- hello-world/src/components/Body.vue
+++ hello-world/src/components/Body.vue
@@ -1,3 +1,9 @@
 <template>
-    <h1></h1>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <img src="./../assets/Books pic.png" class="image_books">
+            </div>
+        </div>
+    </div>
 </template>
