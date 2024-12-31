0045de3183df1636cd5e435f162d07b8a4d155f6
Boukind Rachid <boukind.r1@gmail.com>
2024-12-31 12:22:40 +0100

    simplifed client side validation

--- hello-world/src/components/Login.vue
+++ hello-world/src/components/Login.vue
@@ -4,7 +4,7 @@
             <div class="col">
                 <h1 class="mt-5">Login</h1>
                 <hr>
-                <form-tag @myevent="sumbitHandler">
+                <form-tag @myevent="sumbitHandler" name="myform" event="myevent">
 
                     <text-input
                         v-model="email"
@@ -50,27 +50,6 @@ export default {
         sumbitHandler() {
             console.log("SubmitHandler called success")
         }
-    },
-    mounted() {
-        (function () {
-            'use strict'
-
-            // Fetch all the forms we want to apply custom Bootstrap validation styles to
-            var forms = document.querySelectorAll('.needs-validation')
-
-            // Loop over them and prevent submission
-            Array.prototype.slice.call(forms)
-                .forEach(function (form) {
-                form.addEventListener('submit', function (event) {
-                    if (!form.checkValidity()) {
-                    event.preventDefault()
-                    event.stopPropagation()
-                    }
-
-                    form.classList.add('was-validated')
-                }, false)
-                })
-            })()
     }
 }
 </script>
