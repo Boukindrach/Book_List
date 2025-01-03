dc711403ff38956aa32bf5ef970ec8c47d8fb14b
Boukind Rachid <boukind.r1@gmail.com>
2025-01-03 12:50:26 +0100

    connecting to  database using  driver package

--- vue-api/cmd/api/main.go
+++ vue-api/cmd/api/main.go
@@ -5,16 +5,18 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"vue-api/internal/driver"
 )
 
 type config struct {
-	port int
+	port int // what port do we want the web server to listen on
 }
 
 type application struct {
 	config   config
 	infoLog  *log.Logger
 	errorLog *log.Logger
+	db       *driver.DB
 }
 
 func main() {
@@ -24,13 +26,20 @@ func main() {
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
+	dsn := "host=localhost port=5432 user=postgres password=password dbname=vueapi sslmode=disable timezone=UTC connect_timeout=5"
+	db, err := driver.ConnectPostgres(dsn)
+	if err != nil {
+		log.Fatal("Cannot connect to database")
+	}
+
 	app := &application{
 		config:   cfg,
 		infoLog:  infoLog,
 		errorLog: errorLog,
+		db:       db,
 	}
 
-	err := app.serve()
+	err = app.serve()
 	if err != nil {
 		log.Fatal(err)
 	}
